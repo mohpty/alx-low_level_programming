da0fb9952beefb46d479acfd2278eb926a34f319
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 00:18:05 +0200

    god dammit, betty is a tough one, no.5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints a sentence
+ * Return: 0
+ */
+int main(void)
+{
+	/* print the sentence */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
