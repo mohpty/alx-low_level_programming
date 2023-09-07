a67e6c3e5b96c808762d4ee1c9f77e770aae9a62
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 01:14:10 +0200

    last advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"%s%s",
+	fprintf(stderr, "and that piece of art is useful\" %s, %s",
 			"- Dora Korpar", "2015-10-19\n");
-	return (0);
+	return (1);
 }
