17e7517ea53d814fcf8c05f65199c290995e6f86
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 01:11:03 +0200

    last advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the stderr
+ * Return: always 0
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"%s%s",
+			"- Dora Korpar", "2015-10-19\n");
+	return (0);
+}
