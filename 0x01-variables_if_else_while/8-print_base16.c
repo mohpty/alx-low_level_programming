0ec683adab0294ec9ec0f5862e35e2b330021ddb
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 15:54:23 +0200

    solving 9th one

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print digits from 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 97 ; i < 103 ; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
