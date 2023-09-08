25b85eb4794b272c9b01d2af964a36ea02cf289a
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 15:25:54 +0200

    solving 4th one

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in one line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i < 91 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
