9b71ef1c4b199b3dba7a6961159e2dbe5f62b16b
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 15:04:32 +0200

    solving first task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - choose a random number and check if it's positive,
+ * negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n);
+	if (n == 0)
+	{
+		printf(" is zero\n");
+	}
+	else if (n > 0)
+	{
+		printf(" is positive\n");
+	}
+	else
+	{
+		printf(" is negative\n");
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+2nd c programming project
