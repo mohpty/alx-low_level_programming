9eabc91037d00ed6d20af23044955617a58a6b80
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-08 15:50:12 +0200

    solving 8th one

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122 ; i > 96 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
