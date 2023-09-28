74cc38caf4e6f7db7c314f874c7de235825962d0
Mohammad Fathi <mohfabdou@gmail.com>
2023-09-28 20:31:38 +0200

    fixing betty issues

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calculate square root of a number
+ * _sqrtrec - calculate square root of a number
  * @n: the number
+ * @i: the iterator
  * Return: square root of a number
  */
 int _sqrtrec(int n, int i)
 {
-	
+
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
@@ -16,6 +17,11 @@ int _sqrtrec(int n, int i)
 	return (_sqrtrec(n, i + 1));
 }
 
+/**
+ * _sqrt_recursion - find sqrt of a number
+ * @n: the number
+ * Return: the square root or -1
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,6 +5,7 @@
  * _isprime - actively check if a number is prime
  * @n: the number
  * @i: iterator
+ * Return: 1 if prime, 0 otherwise.
  */
 int _isprime(int n, int i)
 {
@@ -12,18 +13,18 @@ int _isprime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return _isprime(n, i - 1);
+	return (_isprime(n, i - 1));
 }
 
 /**
  * is_prime_number - check if the number is prime
  * @n: the number
- * return: 1 if prime, 0 otherwise.
+ * Return: 1 if prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return _isprime(n, n-1);
+	return (_isprime(n, n - 1));
 
 }
