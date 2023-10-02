ecc829182cfe50673b05f626efd72c5297617b30
Mohammad Fathi <mohfabdou@gmail.com>
2023-10-02 21:41:19 +0200

    new one

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * _puts - print a char
  * @str: string to be printed
@@ -7,13 +6,4 @@
  */
 void _puts(char *str)
 {
-	char c = '\n';
-	char *p = &c;
-
-	while (*str != '\0')
-	{
-		write(1, str, 1);
-		str++;
-	}
-	write(1, p, 1);
 }
