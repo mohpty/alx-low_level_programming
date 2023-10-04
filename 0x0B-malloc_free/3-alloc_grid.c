2f7188222c48038fc10c39909517dc93092eb64e
Mohammad Fathi <mohfabdou@gmail.com>
2023-10-04 23:57:13 +0200

    what is left

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,8 +9,35 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int *arr[width];
-	int p* = arr;
+	int **arr;
+	int i, j;
 
-	return (p);
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	arr = (int **) malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+			{
+				free(arr[j]);
+			}
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
 }
