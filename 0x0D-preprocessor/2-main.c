aa6bf08dd4ab5437521d4ebdabca91b95aa6e64b
Mohammad Fathi <mohfabdou@gmail.com>
2023-10-10 22:45:13 +0200

    again

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print file name
- * Return: always 1
+ * main - print file name in the house of sally
+ *
+ * Return: always 0
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (1);
+	return (0);
 }
